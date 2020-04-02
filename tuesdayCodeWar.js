f7417aa334f0fbf01b3eb2bf13e0cf2233a5634c
zikrehaimanot <zikrehaimanot@gmail.com>
2020-04-01 21:24:39 -0400

    edit

--- tuesdayCodeWar.js
+++ tuesdayCodeWar.js
@@ -9,7 +9,6 @@
 // Task
 // Given an integral number of watermelons w (1 ≤ w ≤ 100; 1 ≤ w in Haskell), check whether Pete and Billy can divide the melons so that each of them gets an even amount.
 
-
 function divide(weight){
   if(weight == 2)
     return false;
